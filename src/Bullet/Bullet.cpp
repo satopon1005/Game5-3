5f8e09570bb55f03c8b012d3c8e0178640e58f82
RoswelHelkaiser <144640067+RoswelHelkaiser@users.noreply.github.com>
2024-05-07 17:06:08 +0900

    SwordSlash
    斬撃の大元を作成

--- src/Bullet/Bullet.cpp
+++ src/Bullet/Bullet.cpp
@@ -1,62 +1,50 @@
 #include "Bullet.h"
 
-void Bullet::Init() {
-	Bullet:: bulletType = BULLET_TYPE::NOTHING;
-
-	/*bulletPos = { 0.0f,0.0f,0.0f };
-	bulletRot = { 0.0f,0.0f,0.0f };
-	bulletSpeed = { 0.0f,0.0f,0.0f };
-
-	for (int i = 0; i < BULLET_NUM; i++) {
-		handle = LoadGraph(bulletFilePath[i]);
-	}
-
-	Time = 0.0f;
-	isUse = false;
-	isHit = false;*/
+//コンストラクタ
+CBulletMaster::CBulletMaster()
+{
+	Frame = 0;
 }
 
-void Bullet::Step() {
-	ScreenOut();
-	switch (bulletType) {
-	case NOTHING:
-		break;
-	case RESIDUE:
-
-		break;
-	default:
-		break;
-	}
-}
-
-void Bullet::Draw() {
+//デストラクタ
+CBulletMaster::~CBulletMaster()
+{
 
 }
 
-void Bullet::Fin() {
-	for (int i = 0; i < BULLET_NUM; i++) {
-		DeleteGraph(handle);
+//初期化
+void CBulletMaster::Init()
+{
+	//フレームを初期化
+	Frame = 0;
+}
+
+//ロード
+void CBulletMaster::Load()
+{
+
+}
+
+//通常処理
+void CBulletMaster::Step()
+{
+	//フレームをカウント
+	Frame++;
+	if (Frame >= FRAME_RATE)
+	{
+		//フレームカウントをリセット
+		Frame = 0;
 	}
 }
 
+//描画
+void CBulletMaster::Draw()
+{
 
-void Bullet::ScreenOut() {
-	if (SCREEN_SIZE_X < bulletPos.x) {
-		isUse = false;
-	}
-	if (SCREEN_SIZE_Y < bulletPos.y) {
-		isUse = false;
-	}
 }
 
-int ScopeRand(int min, int max) {
-	int rand;
+//後処理
+void CBulletMaster::Fin()
+{
 
-	rand = GetRand(max);
-
-	if (rand <= min) {
-		return ScopeRand(min, max);
-	}
-	else
-		return rand;
 }
